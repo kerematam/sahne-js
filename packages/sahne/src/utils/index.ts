c080f0cc0fa310ba5583e9f3caab252415a92170
Kerem ATAM <kerematam@gmail.com>
2024-06-30 15:43:55 +0300

    fix: do not include search param while url matching

--- packages/sahne/src/utils/index.ts
+++ packages/sahne/src/utils/index.ts
@@ -323,8 +323,9 @@ export const handleRequestConfig = async ({
 	next?: Match | Match[];
 	onRequest?: CommonConfig['onRequest'];
 }): Promise<boolean | undefined> => {
-	const url = request.url();
-	const parsedUrl = new URL(url);
+	const rawUrl = request.url();
+	const parsedUrl = new URL(rawUrl);
+	const url = `${parsedUrl.origin}${parsedUrl.pathname}`;
 	const baseUrl = parsedUrl.origin;
 
 	const ignoreRule = handleMatch({ baseUrl, url, match: ignore, request });
